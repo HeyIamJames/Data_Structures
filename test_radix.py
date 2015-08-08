fda67eeca4143284974fd85fe8bc3ec5f0680026
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-08 15:06:53 -0700

    adding reversed, lengths, sorted tests

--- test_radix.py
+++ test_radix.py
@@ -12,6 +12,29 @@ def test_randoms():
     assert x == y
 
 
+def test_empty():
+    x = []
+    y = x
+    radixsort(x)
+    assert x == y
+
+
+def tset_reversed():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    y = reversed(x)
+    z = radixsort(y)
+    assert z == x
+
+
+def test_ordered():
+    pass
+
+
 def test_string():
     with pytest.raises(ValueError):
         radixsort('cat')
+
+
+#test non valid inputs,different lengths of integers, already sorted
