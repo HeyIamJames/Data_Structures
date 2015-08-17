b5193f6d30a8032f06fc694431c1498bb57a890b
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-17 13:09:48 -0700

    cleaning up files

--- merge_sort.py
+++ merge_sort.py
@@ -2,6 +2,16 @@ from timeit import timeit
 from random import shuffle
 
 
+def mergesort(l):
+    if len(l) <= 1:
+        return l
+    else:
+        middle = len(l) // 2
+        left = l[:middle]
+        right = l[middle:]
+        return merge(mergesort(left), mergesort(right))
+
+
 def merge(left, right):
     merged = []
     i, j = 0, 0
