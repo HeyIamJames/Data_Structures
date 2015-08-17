336c00f7de703ba4a2eb644e5d4a264f4d52513c
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-17 13:54:00 -0700

    added tests for reveresed, random, non valid inputs, sorted and strings

--- test_sorting_quciksort.py
+++ test_sorting_quciksort.py
@@ -1,12 +1,65 @@
 from sorting_quicksort import quicksort
+from random import shuffle
 import pytest
 
 
-@pytest.fixture
-def test_list():
-    y = [5, 6, 4, 10, 17, 13, 99, 1, 50]
-    return y
+def test_randoms():
+    x = range(100)
+    randoms = x[:]
+    shuffle(randoms)
+    quicksort(randoms)
+    y = randoms
+    assert x == y
 
 
-def test_sort(test_list):
-    assert quicksort(test_list) == [1, 4, 5, 6, 10, 13, 17, 50, 99]
+def test_string():
+    with pytest.raises(TypeError):
+        quicksort('cat')
+
+
+def test_empty():
+    x = []
+    y = x
+    quicksort(x)
+    assert x == y
+
+
+def test_reversed():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    y = []
+    for i in reversed(x):
+        y.append(i)
+    quicksort(y)
+    z = y
+    assert z == x
+
+
+def test_ordered():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    quicksort(x)
+    z = x
+    assert z == x
+
+
+def test_various_length():
+    x = [1, 200, 600, 1234567, 99999, 7]
+    quicksort(x)
+    y = x
+    expected = [1, 7, 200, 600, 99999, 1234567]
+    assert expected == y
+
+
+def test_badinput():
+    with pytest.raises(TypeError):
+        quicksort('bad', 123, [6, 7], ('hi', 2))
+
+
+def test_best():
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    quicksort(x)
+    expected = sorted(x)
+    assert expected == x
