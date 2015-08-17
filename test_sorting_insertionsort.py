421274de517b843c72fa98ac67ba494341b6f290
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-17 13:11:40 -0700

    adding insertion sort tests

---
+++ test_sorting_insertionsort.py
@@ -0,0 +1,58 @@
+from sorting import insertion_sort
+from random import shuffle
+import pytest
+
+
+def test_randoms():
+    x = range(100)
+    randoms = x[:]
+    shuffle(randoms)
+    insertion_sort(randoms)
+    y = randoms
+    assert x == y
+
+
+def test_string():
+    with pytest.raises(TypeError):
+        insertion_sort('cat')
+
+
+def test_empty():
+    x = []
+    y = x
+    insertion_sort(x)
+    assert x == y
+
+
+def test_reversed():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    y = []
+    for i in reversed(x):
+        y.append(i)
+    insertion_sort(y)
+    z = y
+    assert z == x
+
+
+def test_ordered():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    insertion_sort(x)
+    z = x
+    assert z == x
+
+
+def test_various_length():
+    x = [1, 200, 600, 1234567, 99999, 7]
+    insertion_sort(x)
+    y = x
+    expected = [1, 7, 200, 600, 99999, 1234567]
+    assert expected == y
+
+
+def test_badinput():
+    with pytest.raises(TypeError):
+        insertion_sort('bad', 123, [6, 7], ('hi', 2))
