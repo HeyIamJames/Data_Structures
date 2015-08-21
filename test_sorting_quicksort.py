dce1d1ebf055c8b23a68c59c54281fa99ed82fba
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-20 21:13:15 -0700

    added tests for string, empty, reversed, ordered

--- test_sorting_quicksort.py
+++ test_sorting_quicksort.py
@@ -9,3 +9,36 @@ def test_randoms():
     quicksort(randoms)
     y = randoms
     assert x == y
+
+
+def test_string():
+    with pytest.raises(TypeError):
+        quicksort('cat')
+
+
+def test_empty():
+    x = []
+    y = x
+    quicksort(x)
+    assert x == y
+
+
+def test_reversed():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    y = []
+    for i in reversed(x):
+        y.append(i)
+    quicksort(y)
+    z = y
+    assert z == x
+
+
+def test_ordered():
+    x = []
+    for i in range(1, 10):
+        x.append(i)
+    quicksort(x)
+    z = x
+    assert z == x
