06f0c040c48f098a1a83076721ce2148f742babb
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-20 21:08:55 -0700

    adding main quicksort function and helper function

--- sorting_quicksort.py
+++ sorting_quicksort.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 
 
+def quicksort(_list):
+    if len(_list) > 1:
+        helper(_list, 0, len(_list) - 1)
+    else:
+        return _list
+
+
+def helper(_list, first, last):
+    if first < last:
+        splitpoint = partition(_list, first, last)
+        helper(_list, first, splitpoint - 1)
+        helper(_list, splitpoint + 1, last)
+
+
 def partition(_list, first, last):
     pivot = _list[first]
     leftcounter = first + 1
@@ -21,3 +35,9 @@ def partition(_list, first, last):
             temp = _list[leftcounter]
             _list[leftcounter] = _list[rightcounter]
             _list[rightcounter] = temp
+
+    temp = _list[first]
+    _list[first] = _list[rightcounter]
+    _list[rightcounter] = temp
+
+    return rightcounter
